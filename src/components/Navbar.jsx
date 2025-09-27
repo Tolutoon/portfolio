f592e7bf919edf68a5145f1877b6d99dbdee3f4a
Olopade Tolu <getolopadetolu@gmail.com>
2025-09-27 06:36:59 +0100

    changes made

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,6 +53,7 @@ const Navbar = () => {
         {/* Menu links */}
         <Link to="/" className='menu-link'>Home</Link>
         <Link to="/projects" className='menu-link'>Projects</Link>
+        <Link to="/projects" className='menu-link'>Resume</Link>
         <a href="mailto: johndoe@gmail.com" className='menu-link'>Contact</a>
       </div>
     </>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -90,8 +90,8 @@ const Hero = () => {
 
         {/* Text container */}
         <div className="main-container h-screen flex flex-col lg:justify-center items-start lg:py-12 max-lg:pt-40">
-          <h1 className="text-3xl lg:text-[3.2vw] uppercase font-heading font-semibold">Tolu</h1>
-          <h2 className="text-6xl lg:text-[8vw] font-heading font-bold leading-[1] tracking-tight mt-3 mb-6">Flutter Dev <br/> & <span className='text-stroke'> Illustrator </span></h2>
+          <h1 className="text-2xl lg:text-[3.2vw] uppercase font-heading font-semibold">Tolu</h1>
+          <h2 className="text-6xl lg:text-[8vw] font-heading font-bold leading-[1] tracking-tight mt-3 mb-6">Flutter Dev <br/> & <span className='text-stroke'> Illustrator. </span></h2>
           <GradientButton text="Let's Talk" link="mailto:john@gmail.com" className="gradient-btn" />
         </div>
 
