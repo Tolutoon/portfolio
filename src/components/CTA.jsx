7f4fe3bbb048a4b42d43883708ccea158deb2695
Olopade Tolu <getolopadetolu@gmail.com>
2026-01-11 14:54:10 +0100

    chnages made

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -26,7 +26,7 @@ const CTA = () => {
     <>
       <div ref={ctaRef} className="transition-colors duration-300 mix-blend-difference">
         <div className="main-container py-20 lg:py-28 h-full flex flex-col gap-8 justify-center items-center">
-          <h4 className='max-w-6xl text-2xl md:text-3xl xl:text-[40px] 2xl:text-5xl text-center leading-[1.25] mix-blend-difference'>Freelance projects, collaborations and full-time opportunities. Let's get acquainted</h4>
+          <h4 className='max-w-6xl text-2xl md:text-3xl xl:text-[40px] 2xl:text-5xl text-center leading-[1.25] mix-blend-difference'>Projects, collaborations and full-time opportunities. Let's get acquainted</h4>
           <GradientButton text="Book a Call" link="/" />
         </div>
       </div>
